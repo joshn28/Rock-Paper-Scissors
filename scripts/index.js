b580d08fe8119308b04375eca1c8b8663cecd879
joshn28 <jono2td8@gmail.com>
2021-09-06 18:35:46 -0400

    Completed game function and restartGame function

--- scripts/index.js
+++ scripts/index.js
@@ -16,27 +16,64 @@ function playRound(playerSelection, computerSelection) {
         case playerSelection === "ROCK" && computerSelection === "SCISSORS":
         case playerSelection === "PAPER" && computerSelection === "ROCK":
         case playerSelection === "SCISSORS" && computerSelection === "PAPER":
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            message.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+            ++humanWins;
+            break;
         case computerSelection === "ROCK" && playerSelection === "SCISSORS":
         case computerSelection === "PAPER" && playerSelection === "ROCK":
         case computerSelection === "SCISSORS" && playerSelection === "PAPER":
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            message.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            ++computerWins;
+            break;
         case playerSelection === computerSelection:
-            return "It's a DRAW!";
+            message.innerText = "It's a DRAW!";
+            break;
     }
 }
 
-const rockBtn = document.querySelector("button[value='rock']");
-rockBtn.addEventListener('click', (e) => {
-    console.log(playRound(e.originalTarget.value, computerPlay()));
-});
+function game() {
+    if (humanWins > 4 || computerWins > 4) {
+        if (humanWins > computerWins) {
+            message.innerText = "Congratulations, You Won!";
+        } else {
+            message.innerText = "Awww, You Lose!";
+        }
+        restartGame();
+    }
+    score.innerText = `Human: ${humanWins} CPU: ${computerWins}`;
+}
 
-const paperBtn = document.querySelector("button[value='paper']");
-paperBtn.addEventListener('click', (e) => {
-    console.log(playRound(e.originalTarget.value, computerPlay()));
-});
+function restartGame() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.style.display = "none";
+    });
+    const playAgainBtn = document.createElement('button');
+    playAgainBtn.innerText = "Continue?";
+    playAgainBtn.addEventListener('click', () => {
+        humanWins = 0;
+        computerWins = 0;
+        message.innerText = "Make a Selection";
+        score.innerText = "Human: 0 CPU: 0";
+        buttons.forEach((button) => {
+            button.style.display = "inline";
+        });
+        document.querySelector('div').removeChild(playAgainBtn);
+    });
+    document.querySelector('div').appendChild(playAgainBtn);
+}
 
-const scissorsBtn = document.querySelector("button[value='scissors']");
-scissorsBtn.addEventListener('click', (e) => {
-    console.log(playRound(e.originalTarget.value, computerPlay()));
+let humanWins = 0;
+let computerWins = 0;
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+
+const selectionBtns = document.querySelectorAll("button");
+selectionBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (humanWins < 5 && computerWins < 5) {
+            playRound(e.originalTarget.value, computerPlay());
+        }
+        game();
+    });
 });
